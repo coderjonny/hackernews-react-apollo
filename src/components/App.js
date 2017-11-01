6a29d1be8617f12ad68d27a3faabdf9e2ae5621d
Jonny Kang <jono.kang@gmail.com>
2017-11-01 12:25:37 -0700

    rendering out inputs for mutating graphQL data

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
 import LinkList from './LinkList'
+import CreateLink from './CreateLink'
 
 class App extends Component {
   render() {
     return (
-      <LinkList />
-    );
+      <div>
+        <LinkList />
+        <CreateLink />
+      </div>
+    )
   }
 }
 
